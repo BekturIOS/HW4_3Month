19b6c5fbbe77991574dbbd2f984d067577dca845
bektur <bektur060525@gmail.com>
2023-03-04 18:38:04 +0600

    Initial Commit

---
+++ HW4_3Month/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW4_3Month
+//
+//  Created by Бектур Каримов on 4/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
